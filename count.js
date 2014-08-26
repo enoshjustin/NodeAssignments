e0da94c3347cb37f653ed6117e1409f5aa9b63a1
enoshjustin <justinenosh@gmail.com>
2014-08-26 10:01:28 +0530

    Assignment4

--- count.js
+++ count.js
@@ -1,9 +1,25 @@
-var fcount= require('fs')
-var count=0;
-fcount.watchFile('text1.txt', function (curr,prev) {
-console.log('Last Checked On : ' + prev.mtime);
-count++;
-console.log('executed :' + count + ' times');
-console.log('Todays time :'+ curr.mtime);
-//console.log('Todays Excecution Count:' + count);
+var fiprint= require ("fs");
+var rawords = require ("random-words");
+
+		fiprint.appendFile('randomtext.txt',rawords()+'\n',function(err1){
+		
+		if(err1) {
+			throw err1;
+			console.log(err1);
+		}
+		else
+			{
+				fiprint.readFile('randomtext.txt','utf8',function(err2,data){
+				if(err2){
+					throw err2;
+				}
+				else{
+					var res = data.split('\n');
+					// console.log("Words Added : \n" + res);
+					var len = res.length-1; 
+					console.log("The Counter is Updated to : " + len);
+					console.log("File Updated");
+			}
+		})
+	}
 });
